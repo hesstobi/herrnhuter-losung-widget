d129a4a253fd8edece3b01cafd16c092bb6ff131
Benjamin Pick <benjaminpick@github.com>
2014-03-15 14:28:33 +0100

    refactor view

--- views/widget_options.php
+++ views/widget_options.php
@@ -1,14 +1,9 @@
-<?php
-	echo '<p>';
-    echo  '<label for="' . $this->get_field_id('title') . '">';
-    echo  'Titel:</label>';
-    echo  '<input style="width: 100%;" id="' . $this->get_field_id('title') . '" name="' . $this->get_field_name('title') . '" type="text" value="' . $instance['title'] . '" />';
-    echo  '</p>';
+<p>
+	<label for="<?php echo $this->get_field_id('title') ?>">Titel:</label>
+   	<input style="width: 100%;" id="<?php echo $this->get_field_id('title') ?>" name="<?php echo $this->get_field_name('title') ?>" type="text" value="<?php echo esc_attr($instance['title']); ?>" />
+</p>
 	
-	echo '<p>';
-	echo '<input class="checkbox" type="checkbox" ';
-	if ($instance['showlink'])
-		echo 'checked="checked" ';
-	echo 'id="' . $this->get_field_id( 'showlink' ) . '" name="' .  $this->get_field_name( 'showlink' ) . '" />';
-	echo '<label for="' . $this->get_field_id( 'showlink' ) . '"> Zeige Link zu Bibleserver.com</label>';
-	echo '</p>';
+<p>
+	<input class="checkbox" type="checkbox" <?php if ($instance['showlink']) echo 'checked="checked" '; ?> id="<?php echo $this->get_field_id( 'showlink' ) ?>" name="<?php echo $this->get_field_name( 'showlink' ) ?> " />
+	<label for="<?php echo $this->get_field_id( 'showlink' ) ?>">Zeige Link zu Bibleserver.com</label>
+</p>
